56501e6c0054e05ad3a3284a352c31ae5aa565f5
Andrei4 <meliciron@mail.ru>
2020-02-19 01:18:21 +0700

    Класс с главным окном переименован

--- NoteAppUI/MainForm.cs
+++ NoteAppUI/MainForm.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace NoteAppUI
 {
-    public partial class UserControl1: UserControl
+    public partial class MainForm: UserControl
     {
-        public UserControl1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
